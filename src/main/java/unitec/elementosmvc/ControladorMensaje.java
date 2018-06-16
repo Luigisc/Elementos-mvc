a5c16f59eccd7400074e7cbd5294e3bf4e4ff28a
T-107 <T-107@PC205.mshome.net>
2018-06-15 21:42:59 -0500

    REST 15-06-18

--- src/main/java/unitec/elementosmvc/ControladorMensaje.java
+++ src/main/java/unitec/elementosmvc/ControladorMensaje.java
@@ -1,7 +1,10 @@
 package unitec.elementosmvc;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author T-107
  */
-
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
 public class ControladorMensaje {
-    @GetMapping("/hola")
-    public String algo(){
-        return "Hola desde un Controlador";
+
+    @Autowired
+    RepositorioMensaje repomensa;
+
+//Caso a): Buscar todos  
+    @GetMapping("/mensaje")
+    public List<Mensaje> buscarTodos() {
+        return repomensa.findAll();
+    }
+
+    //caso b): Buscar por ID   las llaves en --->{id}<--- significa que el usuario necesita introducir un valor, 
+    //conocida como variable de ruta
+    @GetMapping("/mensaje/{id}")
+    public Mensaje buscarPorID(@PathVariable String id) {
+        Mensaje mensa = new Mensaje();
+        return repomensa.findById(id).get();
     }
 }
